99da1be47b503f987a3a9a2bc42586b6b84a265a
Morris Allison III <morris@yola.com>
2016-04-14 11:09:12 -0700

    Remove unused files

--- src/paginators/index.js
+++
@@ -1,9 +0,0 @@
-import BasePaginator from './base-paginator';
-import LimitOffsetPaginator from './limit-offset-paginator';
-import PageNumberPaginator from './page-number-paginator';
-
-export default {
-  BasePaginator,
-  LimitOffsetPaginator,
-  PageNumberPaginator
-};
--- src/query-handlers/index.js
+++
@@ -1,7 +0,0 @@
-import LimitOffsetQueryHandler from './limit-offset-query-handler';
-import PageNumberQueryHandler from './page-number-query-handler';
-
-export default {
-  LimitOffsetQueryHandler,
-  PageNumberQueryHandler
-};
